119cb8f1b46b6b1ef607907ce1152774d80dc1e8
DevSwayam <99035115+DevSwayam@users.noreply.github.com>
2022-03-05 20:58:52 +0530

    Delete ocs.js

--- ocs.js
+++
@@ -1,9 +0,0 @@
-
-function onclickmenu() {
-    document.getElementById("hambergermenu").classList.toggle("icon")
-    document.getElementById("nav-list").classList.toggle("change")
-}
-$("button").click(function () {
-    $(".hambergermenu").toggleClass("icon");
-    $(".nav-list").toggleClass("change");
-});
